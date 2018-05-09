02aacfb8c2762328cc126f382d2b8a80dfab67ef
mobiusxp <kho@dnguyen.me>
2018-05-09 18:08:20 -0400

    Added form validation through Vue

--- js/app.js
+++ js/app.js
@@ -68,12 +68,37 @@ var app = new Vue({
 				"WV": "West Virginia",
 				"WI": "Wisconsin",
 				"WY": "Wyoming"
-			}
-		
+			},
+		address1: null,
+		address2: null,
+		city: null,
+		state: null,
+		zip: null,
+		errors: []
 	},
 	methods: {
+		validate: function(){
+			this.errors = [];
+			if(!this.address1){
+				this.errors.push("Address1 cannot be blank");
+			}
+			if(!this.address2){
+				this.errors.push("Address2 cannot be blank");
+			}
+			if(!this.city){
+				this.errors.push("City cannot be blank");
+			}
+			if(this.state == "Choose..." || !this.state){
+				this.errors.push("State cannot be blank");
+			}
+			if(!this.zip){
+				this.errors.push("ZIP cannot be blank");
+			}else if(!this.zip.match(/^\d{5}(?:[-\s]\d{4})?$/)){
+				this.errors.push("ZIP must be 5 digits or 5+4 digits (ex. 12345-1234)");
+			}
+		},
 		say: function(text){
-			alert(text)
+			alert(this.address1);
 		}
 	}
 })
